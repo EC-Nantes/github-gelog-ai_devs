189ad1a545290b194bd91c51da46ab8a92c48dbf
anas- <anas-@130.66.212.37>
2025-09-19 09:47:45 +0200

    La toute première version du test

---
+++ Git_test/src/main/java/org/centrale/git_test/Git_test.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.git_test;
+
+/**
+ *
+ * @author anas-
+ */
+public class Git_test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
